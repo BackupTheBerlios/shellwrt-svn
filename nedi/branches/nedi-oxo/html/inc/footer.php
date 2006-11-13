bf3251f8f6eef2f1283e6195696ecdf63de84ff2
oxo <oxo@ecc17a00-d921-0410-9e81-ae938e6ed21a>
2006-11-13 16:18:56 +0000

    remove nedi

--- nedi/branches/nedi-oxo/html/inc/footer.php
+++
@@ -1,4 +0,0 @@
-<p>
-<table bgcolor=#000000 cellspacing=1 cellpadding=4 width=100% border=0><tr bgcolor=#<?php echo "$bg1" ?>>
-<td align=right><font size=-2>&copy; 2001-2006 Remo Rickli</font></td></tr>
-</table></body></html>
